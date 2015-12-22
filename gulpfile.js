8799d7e54390f927b843408ed7c51aeb31ed43a2
chamer <chamer2010@gmail.com>
2015-12-22 11:27:03 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var inject = require('gulp-inject');
+var target = gulp.src('index.html');
+
+// inject task, is used to automatically inject css files to index page
+gulp.task('inject-css', function () {
+
+  var sources = gulp.src(['bower_components/angular-material/angular-material.css', 'app/styles/*.css'], {read: false});
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./'));
+
+});
+
+// inject task, is used to automatically inject js files to index page
+gulp.task('inject-js', function () {
+
+  var sources = gulp.src(['bower_components/**/*.min.js', 'app/**/*.js'], {read: false});
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./'));
+
+});
+
+// serve task, used to init browser sync module
+gulp.task('serve', ['inject-css', 'inject-js'],  function () {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+});
